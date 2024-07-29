9ebbc807de2fb99035c2b90b43b9cf70c4654691
= <=>
2024-07-29 19:09:53 +0530

    Initial Commit

---
+++ src/app/modules/starter/components/starter-home-context/inner-item/starter-main-slider/starter-main-slider.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starter-main-slider',
+  templateUrl: './starter-main-slider.component.html',
+  styleUrl: './starter-main-slider.component.scss'
+})
+export class StarterMainSliderComponent {
+
+}
---
+++ src/app/modules/starter/components/starter-home-context/starter-home-context.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starter-home-context',
+  templateUrl: './starter-home-context.component.html',
+  styleUrl: './starter-home-context.component.scss'
+})
+export class StarterHomeContextComponent {
+
+}
